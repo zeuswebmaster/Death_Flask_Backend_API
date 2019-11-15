4e37dc6e54e07c34260d870786e3d0e7086b6be8
valentine <vbabrouski@outlook.com>
2019-11-15 09:59:36 +0300

    report_crawler init

--- app/main/util/parsers.py
+++
@@ -1,9 +0,0 @@
-import werkzeug
-from flask_restplus import reqparse
-
-file_upload = reqparse.RequestParser()
-file_upload.add_argument('csv_file',
-                         type=werkzeug.datastructures.FileStorage,
-                         location='files',
-                         required=True,
-                         help='CSV file')
