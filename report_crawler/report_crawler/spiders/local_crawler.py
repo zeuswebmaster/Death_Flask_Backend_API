7d1d8041f95d2808f41ceb80aa3e03b688b3751c
valentine <vbabrouski@outlook.com>
2019-11-21 22:44:56 +0300

    days delinquent update

--- report_crawler/report_crawler/spiders/local_crawler.py
+++ report_crawler/report_crawler/spiders/local_crawler.py
@@ -1,5 +1,6 @@
 import datetime
 import sqlite3
+import geopy
 
 import scrapy
 
@@ -31,13 +32,21 @@ class Crawler(scrapy.Spider):
             row['collector_account'] = None
             row['last_debt_status'] = None
             row['bureaus'] = None
+            payment_status = self.get_cell(table, "./descendant::b[contains(text(), 'Payment Status')]/ancestor::tr[@class='crTableHeader']/td[{}]/text()", [2, 3, 4])
+            if payment_status == 'current':
+                row['days_delinquent'] = 0
+            elif payment_status == 'late 30 days':
+                row['days_delinquent'] = 30
+            else:
+                row['days_delinquent'] = ''  # set for collection/chargeoff
+            self.log(row['days_delinquent'])
             row['balance_original'] = ''
             row['payment_amount'] = float(self.get_cell(table, "./descendant::b[contains(text(), 'Payment Amount')]/ancestor::tr[@class='crTableHeader']/td[{}]/text()", [2, 3, 4]).replace('$', ''))
             row['last_update'] = datetime.datetime.now()
-            row['graduation'] = self.get_cell(table, "./descendant::b[contains(text(), 'Account Description')]/ancestor::tr[@class='crLightTableBackground']/td[{}]/text()", [2, 3, 4])
-            self.log('Graduation: ', row['graduation'])
+            # row['graduation'] = self.get_cell(table, "./descendant::b[contains(text(), 'Account Description')]/ancestor::tr[@class='crLightTableBackground']/td[{}]/text()", [2, 3, 4])
+            # self.log('Graduation: ', row['graduation'])
             self.insert_row(row)
-            self.log(row)
+            # self.log(row)
 
     def get_cell(self, table_el, row_xpath, params):
         for param in params:
@@ -49,6 +58,6 @@ class Crawler(scrapy.Spider):
     def insert_row(self, row):
         columns = ', '.join(row.keys())
         placeholders = ', '.join('?' * len(row))
-        self.log("INSERT INTO reports ({}) VALUES ({})".format(columns, placeholders))
+        # self.log("INSERT INTO reports ({}) VALUES ({})".format(columns, placeholders))
         self.cursor.execute("INSERT INTO reports ({}) VALUES ({})".format(columns, placeholders), tuple(row.values()))
         self.connection.commit()
